b31c61f699e943234177e82eeda8201c73b1d04b
Taejin Kim <rlaxowls1316@naver.com>
2023-12-10 14:53:22 +0900

    [style]: 헤더 (로그인) 스타일 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
+    <div className="font-bold w-fit ml-auto">
       <Link to="/login">로그인</Link>
     </div>
   );
