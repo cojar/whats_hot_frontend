ee44688d76003abbe13cd67b89936df3a1c574ca
Taejin Kim <rlaxowls1316@naver.com>
2023-12-11 09:51:23 +0900

    [style]: 로그인 버튼 색상 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   const location = useLocation();
   console.log(location.pathname);
   return (
-    <div className="font-bold w-fit ml-auto">
+    <div className="font-bold w-fit ml-auto text-primary">
       {location.pathname !== "/login" && <Link to="/login">로그인</Link>}
     </div>
   );
