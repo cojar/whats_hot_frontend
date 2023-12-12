b07271b2e30694123613d17abc20fac04b2020aa
Taejin Kim <rlaxowls1316@naver.com>
2023-12-12 10:53:53 +0900

    [style]: Home의 header부분 span 태그를 b 태그로 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <div className="mt-8">
         <p className="text-3xl">
-          <span className="text-primary">맛집, 숙박, 여행지</span> 리뷰
+          <b className="text-primary">맛집, 숙박, 여행지</b> 리뷰
         </p>
         <p className="mt-2 text-secondary">
           로그인을 하면 더 많은 기능을 이용할 수 있어요!
