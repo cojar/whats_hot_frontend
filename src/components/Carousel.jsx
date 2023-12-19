7ab55f2f4f9ed55d9ab64a852c4c4af31d6ebb4d
Taejin Kim <rlaxowls1316@naver.com>
2023-12-19 22:58:49 +0900

    [refactor]: 불필요한 useState 사용 제거

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 import CarouselItem from "./CarouselItem";
 
 export default function Carousel({ mockItems }) {
-  //임시 변수
-  const [items, setItems] = useState(mockItems);
-
   return (
     <>
       {/* 맛집 */}
@@ -13,7 +10,7 @@ export default function Carousel({ mockItems }) {
         <p className="text-xl font-bold mb-3">현재 인기 많은 맛집</p>
       </div>
       <ul className="carousel carousel-center w-full space-x-3 ">
-        {items.map((item, i) => {
+        {mockItems.map((item, i) => {
           return (
             <CarouselItem key={item.id} title={item.title} cover={item.cover} />
           );
