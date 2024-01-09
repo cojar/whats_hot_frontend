c0a6137c89583815b91ddebeef0160427115e15c
wooyoung0407 <wooyoung0407@naver.com>
2024-01-09 20:03:10 +0900

    data 해결중

--- src/components/RecommendedRestaurant.jsx
+++ src/components/RecommendedRestaurant.jsx
@@ -8,7 +8,7 @@ export default function RecommendedRestaurant() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://whb.pintor.dev/api/spots");
+        const response = await fetch("/api/spots");
         const data = await response.json();
         setSpots(data.data.list);
       } catch (error) {
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+export default defineConfig({
   plugins: [react()],
-};
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://whb.pintor.dev',
+        changeOrigin: true,
+      },
+    },
+  },
+});
