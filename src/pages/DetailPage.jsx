c2b675f8524b441153fe4f60aa1228b1a9985b5d
wooyoung0407 <wooyoung0407@naver.com>
2024-01-15 15:29:55 +0900

    DetailPage수정

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 import Detail from "../components/Detail";
 import Review from "../components/Review";
-import ReviewWrite from "./ReviewWrite";
 
 export default function DetailPage() {
-  const { id } = useParams();
-  const [reviews, setReviews] = useState([]);
 
   return (
     <>
       <Detail />
-      <Review reviews={reviews} setReviews={setReviews} />
-      <ReviewWrite id={id} reviews={reviews} setReviews={setReviews} />
+      <Review />
     </>
   );
 }
