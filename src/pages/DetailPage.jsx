5281dd24a83e0b2000a813c8997b9e0ee8b4fa76
wooyoung0407 <wooyoung0407@naver.com>
2024-01-15 15:30:50 +0900

    DetailPage재수정

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -3,11 +3,12 @@ import Detail from "../components/Detail";
 import Review from "../components/Review";
 
 export default function DetailPage() {
+  const [reviews, setReviews] = useState([]);
 
   return (
     <>
       <Detail />
-      <Review />
+      <Review reviews={reviews} setReviews={setReviews} />
     </>
   );
 }
